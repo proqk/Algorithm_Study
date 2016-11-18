069a02340546909c960fcb41c0c194b8a814623c
Yujin Lee <proqk@naver.com>
2016-11-18 17:28:32 +0900

    Create 3733.cpp

---
+++ 3733.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MN 10000000
+long long int sum = 0, ss, x, y;
+int s, e, len = 0, flag;
+
+int dp[MN + 1] = { 1,1, };
+
+int change(long long int  k) {
+	int  result;
+	if (k < MN && dp[k] != 0) return dp[k];
+	if (k % 2 == 0) {
+		if (k < MN) {
+			return dp[k]  = change(k / 2)+1;
+		}
+		else {
+			return change(k / 2)+1;
+		}
+	}
+	else{
+		if (k < MN) {
+			return dp[k] = change(k *3 +1 )+1;
+		}
+		else {
+			return change(k * 3 + 1)+1;
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d", &s, &e);
+	for (int i = s; i <= e; i++) {
+		change(i);
+		int q = dp[i];
+		if (q > sum) {
+			ss = i;
+			sum = q;
+		}
+	}
+	printf("%lld %lld", ss, sum);
+}
